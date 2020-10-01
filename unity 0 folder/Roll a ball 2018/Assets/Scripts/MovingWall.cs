736cc602e90b59fc96288ffab13bc5adf078f7cc
Mauritshidde <43985321+Mauritshidde@users.noreply.github.com>
2020-10-01 16:10:04 +0200

    ..
    ...

---
+++ unity 0 folder/Roll a ball 2018/Assets/Scripts/MovingWall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWall : MonoBehaviour
+{
+    [SerializeField] private Vector3 puntA = new Vector3(127, 25, 56);
+    [SerializeField] private Vector3 puntB = new Vector3(127, 25, 26);
+    [SerializeField] private float speed = 2;
+    private float t;
+    private void Update()
+    {
+        t += Time.deltaTime * speed;
+        // Moves the object to target position
+        transform.position = Vector3.Lerp(puntA, puntB, t);
+        // Flip the points once it has reached the target
+        if (t >= 1)
+        {
+            var b = puntB;
+            var a = puntA;
+            puntA = b;
+            puntB = a;
+            t = 0;
+        }
+    }
+    // What Linear interpolation actually looks like in terms of code
+    private Vector3 CustomLerp(Vector3 a, Vector3 b, float t)
+    {
+        return a * (1 - t) + b * t;
+    }
+}
---
+++ unity 0 folder/Roll a ball 2018/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text JumpText;
+    public Text winText;
+    public float jumpForce;
+    public float SuperJumpForce;
+    public GameObject player;
+
+    private Rigidbody rb;
+
+    private int count;
+    private int jumpcount;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jumpcount = 0;
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        SetJumpText();
+
+        if (player.transform.position.y <= -12.37)
+        {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        }
+
+
+
+
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+
+            count += 1;
+            jumpcount += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 14)
+        {
+            winText.text = "You Win!";
+        }
+    }
+    void SetJumpText()
+    {
+        JumpText.text = "Jumps: " + jumpcount.ToString();
+    }
+    
+    void OnCollisionStay(Collision collision)
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (jumpcount >= 1)
+        {
+                if (collision.gameObject.tag == "Ground")
+                {
+                    rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                    jumpcount -= 1;
+                    
+                }
+            }
+        }
+        if (collision.gameObject.tag == "Ground")
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                if (jumpcount >= 3)
+                {
+                    rb.AddForce(Vector3.up * SuperJumpForce, ForceMode.Impulse);
+                    jumpcount -= 3;
+                }
+            }
+        }
+
+        if (collision.gameObject.tag == "Kill")
+        {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        }
+    }
+    IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(5);
+    }
+}
